03e9ad5849190b802ee7871ba1ca569a10dc13ea
Joey Faulkner <joeymfaulkner@gmail.com>
2020-05-19 13:57:45 +0200

    add fake timeseries into PLC and forward them across cloud

--- PLC/PLC/server.py
+++ PLC/PLC/server.py
@@ -3,6 +3,7 @@ import asyncio
 
 from asyncua import ua, Server
 from asyncua.common.methods import uamethod
+from PLC.data_fetcher import TimeSeriesStorage
 
 logging.basicConfig(level=logging.INFO)
 _logger = logging.getLogger('asyncua')
@@ -22,7 +23,25 @@ async def main():
     myvar = await myobj.add_variable(idx, 'MyVariable', 6.7)
     # Set MyVariable to be writable by clients
 
+    # variables that should be able to be set by external client
+    setobj = await server.nodes.objects.add_object(idx, 'Controlling')
+    cntrl_1 = await setobj.add_variable(idx, 'Control1', True)
+    cntrl_2 = await setobj.add_variable(idx, 'Control2', 'hello')
+    cntrl_3 = await setobj.add_variable(idx, 'Control3', 6)
+
+    ts_store = TimeSeriesStorage()
+    tsobj = await server.nodes.objects.add_object(idx, 'TimeSeries')
+    for jdx in range(5):
+        ts = await tsobj.add_variable(idx, f'TimeSeries{jdx}', 0)
+        ts_store.assign_timeseries(ts)
+
     await myvar.set_writable()
+    await cntrl_1.set_writable()
+    await cntrl_2.set_writable()
+    await cntrl_3.set_writable()
+
+    await ts_store.propagate()
+
     _logger.info('Starting server!')
     async with server:
         count = 0
@@ -31,7 +50,7 @@ async def main():
             count += 0.1
             _logger.info('Set value of %s to %.1f', myvar, count)
             await myvar.write_value(count)
-
+            await ts_store.propagate()
 
 if __name__ == '__main__':
     asyncio.run(main())
--- cloud/cloud/forwarding_service.py
+++ cloud/cloud/forwarding_service.py
@@ -20,22 +20,23 @@ async def main():
     await server.init()
     await client.connect()
     obj_1 = await server.nodes.objects.add_object(0, 'ExamplePLC')
+    obj_2 = await server.nodes.objects.add_object(1, 'TSPLC')
     client_1 = await client.nodes.objects.get_child(['0:MyObject'])
+    client_2 = await client.nodes.objects.get_child(['0:TimeSeries'])
 
     server.set_endpoint(cloud_url)
     client_var = await client.nodes.root.get_child(['0:Objects', '0:MyObject', '0:MyVariable'])
     handler = SubscriptionHandler(client, server)
 
     await clone_and_subscribe(client_1, obj_1, handler)
+    await clone_and_subscribe(client_2, obj_2, handler)
     # setup our own namespace, not really necessary but should as spec
     subscription = await client.create_subscription(5, handler)
     nodes = [client_var]
     async with server:
         await subscription.subscribe_data_change(nodes)
         await asyncio.sleep(100)
-
         await subscription.delete()
-
         await asyncio.sleep(1)
         print(await client_var.read_value())
 
