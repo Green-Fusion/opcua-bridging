b1f1ad5e2092d5dc00680ffcaf4599aba7e15285
Joey Faulkner <joeymfaulkner@gmail.com>
2020-09-08 10:31:22 +0200

    remove bullshit from tests

--- examples/bridge/cloud/cloud/yaml-consumption.py
+++ examples/bridge/cloud/cloud/yaml-consumption.py
@@ -21,25 +21,8 @@ cloud_url = "opc.tcp://cloud:4840/freeopcua/server/"
 async def main():
     # setup our serverclone_and_subscribe(client_node, server_node, sub_handler)
     server = await server_from_yaml('cloud_server_config.yaml')
-
-    client = Client(url=PLC_url)
-    await client.set_security(
-        SecurityPolicyBasic256Sha256,
-        certificate_path='/credentials/cloud_cert.der',
-        private_key_path='/credentials/cloud_private_key.pem',
-        server_certificate_path='/credentials/PLC_cert.der'
-    )
-    await client.connect()
-
-    client2 = Client(url=PLC_url_2)
-
-    await client2.connect()
-
-    holder = await bridge_from_yaml(server, '/appdata/test_yaml/test.yaml')
-    for sub_hold in holder:
-        _logger.warning(sub_hold[0]._client_server_mapping)
+    await bridge_from_yaml(server, '/appdata/test_yaml/test.yaml')
     async with server:
-        # await subscription.subscribe_data_change(nodes)
         while True:
             await asyncio.sleep(100)
 
