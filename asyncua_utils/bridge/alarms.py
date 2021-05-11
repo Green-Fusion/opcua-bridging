6a0cdfaaad151e78f3092fe89355c1769165d307
Joey Faulkner <joeymfaulkner@gmail.com>
2021-05-11 16:14:42 +0200

    map to new source node

--- asyncua_utils/bridge/alarms.py
+++ asyncua_utils/bridge/alarms.py
@@ -27,7 +27,7 @@ class AlarmHandler:
     async def event_notification(self, event: Event):
         alarm = self._server.get_node(ua.NodeId(10637))
         alarm_gen = await self._server.get_event_generator(alarm,
-                                                           emitting_node=event.SourceNode,
+                                                           emitting_node=self._node_mapping.get_bridge_id(event.SourceNode),
                                                            notifier_path=[ua.ObjectIds.Server])
         # event.SourceNode = self._server.nodes.server.nodeid
         event.Message = ua.LocalizedText('hello from bridge')
